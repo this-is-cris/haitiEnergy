bc1206f688521277e6bb37ac745cee6e5c79b1e9
Cris Poiata <cristinapoiata@hotmail.com>
2015-02-05 01:23:08 +0100

    Added gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var gulp = require("gulp"),
+		less = require("gulp-less");
+
+gulp.task("less", function() {
+	gulp.src("wp-content/themes/haitiEnergy/styles/less/styles.less")
+			.pipe(less())
+			.pipe(gulp.dest("wp-content/themes/haitiEnergy/styles/css/"));
+});
+
+
+
+gulp.task("default", function() {
+	var lessTasks = ["less"];
+
+	gulp.watch("wp-content/themes/haitiEnergy/styles/less/**/*.less", lessTasks);
+});
